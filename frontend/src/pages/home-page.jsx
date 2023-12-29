0ccbdf4f2ca470111b1ddfdd404d40e0f03bc6b2
David Berry <dberry3755@gmail.com>
2023-12-29 11:50:05 -0500

    chat working, just no knowledge retrieval yet

---
+++ frontend/src/pages/home-page.jsx
@@ -0,0 +1,115 @@
+import React, { useState, useEffect } from "react";
+
+export default function AIResponse({ openModal }) {
+  const key = import.meta.env.VITE_OPENAI_API_KEY;
+  const [inputText, setInputText] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [chatHistory, setChatHistory] = useState([]);
+
+  useEffect(() => {
+    const initialMessages = [
+      {
+        role: "assistant",
+        content:
+          "Hi! I'm Quad, here to chat with you about your warehouse processes and help you with your needs! ☀️",
+      },
+      {
+        role: "assistant",
+        content:
+          "Let's chat about what is going on. In a sentence or two, tell me what challenges you are facing in regards to your warehouse needs. Feel free to be casual like this is a discussion between friends or coworkers.",
+      },
+    ];
+
+    setChatHistory(initialMessages);
+  }, []);
+
+  const handleInputChange = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const appendToChatHistory = (message) => {
+    setChatHistory((prevChatHistory) => [...prevChatHistory, message]);
+  };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+
+    const messageContent = {
+      role: "user",
+      content: inputText,
+    };
+
+    appendToChatHistory(messageContent);
+
+    try {
+      const response = await fetch(
+        "https://api.openai.com/v1/chat/completions",
+        {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${key}`,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            model: "gpt-4",
+            messages: [...chatHistory, messageContent],
+          }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const data = await response.json();
+      const chatResponse = {
+        role: "assistant",
+        content: data.choices[0].message.content,
+      };
+
+      appendToChatHistory(chatResponse);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    } finally {
+      setIsLoading(false);
+      setInputText("");
+    }
+  };
+
+  return (
+    <>
+      <div className="chat-container">
+        <div className="chat-box">
+          <div className="chat-content">
+            {chatHistory.map((message, index) => (
+              <p
+                key={index}
+                className={
+                  message.role === "user" ? "user-message" : "assistant-message"
+                }
+              >
+                {message.content}
+              </p>
+            ))}
+          </div>
+          <div className="input-container">
+            <input
+              type="text"
+              className="chat-input"
+              placeholder="Type a message..."
+              value={inputText}
+              onChange={handleInputChange}
+            />
+            <button
+              className="send-button"
+              onClick={fetchData}
+              disabled={isLoading}
+            >
+              {isLoading ? <div className="loader"></div> : "Send"}
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
