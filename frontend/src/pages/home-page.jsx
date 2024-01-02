ea7871c2a426405ed99920b00bcf43607359d245
David Berry <dberry3755@gmail.com>
2024-01-02 17:13:03 -0500

    fixed the public folder for build

--- frontend/src/pages/home-page.jsx
+++ frontend/src/pages/home-page.jsx
@@ -78,7 +78,7 @@ export default function AIResponse({ openModal }) {
 
   const fetchFileContent = async () => {
     try {
-      const response = await fetch("../assets/knowledge.txt"); 
+      const response = await fetch("../../public/knowledge.txt");
       if (!response.ok) {
         throw new Error("Failed to fetch file");
       }
@@ -153,7 +153,7 @@ export default function AIResponse({ openModal }) {
         <div className="disclaimer-wrapper">
           <div className="disclaimer-container">
             <img
-              src="../assets/logofull.png"
+              src="../../public/logofull.png"
               alt="Quadspace Logo"
               style={{ maxWidth: "100%", height: "auto" }}
             />
@@ -170,7 +170,7 @@ export default function AIResponse({ openModal }) {
       ) : (
         <div className="chat-container">
           <img
-            src="../assets/logofull.png"
+            src="../../public/logofull.png"
             alt="Quadspace Logo"
             style={{
               maxWidth: "80%",
